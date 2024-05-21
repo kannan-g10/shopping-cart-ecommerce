ce676bc072f3e23230df077ce67c9740abd09ec4
Kannan G <gkannan271120@gmail.com>
2024-05-21 21:16:21 +0530

    E-Commerce app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+
+const Main = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
